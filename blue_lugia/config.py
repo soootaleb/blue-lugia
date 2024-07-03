cd5545096645d592c51da48dba055377bdd5a189
soootaleb <sofiane.ext@unique.ch>
2024-07-03 11:20:15 +0200

    config ignore extra

--- blue_lugia/config.py
+++ blue_lugia/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class ModuleConfig(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra='ignore')
 
     ENDPOINT_SECRET: str = ""
     API_KEY: str = ""
