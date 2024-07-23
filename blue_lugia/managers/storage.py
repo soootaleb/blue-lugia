4310aa339427adc353b815b244996d6ce1ab4986
soootaleb <sofiane.ext@unique.ch>
2024-07-23 10:47:18 +0200

    storage set accept list

--- blue_lugia/managers/storage.py
+++ blue_lugia/managers/storage.py
@@ -24,7 +24,7 @@ class StorageManager(Manager):
     def get(self, key: str, default: Any = None) -> Any:
         return self.data.get(key, default)
 
-    def set(self, key: str, value: dict | str | int | bool) -> dict:
+    def set(self, key: str, value: dict | str | int | bool | list) -> dict:
         self._store.update(
             content=self._store.content,
             debug={**self._store.debug, "_store": {**self.data, key: value}},
