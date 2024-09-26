e6b68ee480915a078054a9868181e6b26a1b1de2
zachschillaci27 <zachschillaci27@gmail.com>
2024-09-26 14:23:38 +0200

    Add util method to get version

--- blue_lugia/commands/version.py
+++ blue_lugia/commands/version.py
@@ -8,6 +8,7 @@ import toml
 from blue_lugia.app import App
 from blue_lugia.config import ModuleConfig
 from blue_lugia.state import StateManager
+from blue_lugia.utils import get_version
 
 """
 Returns versions
@@ -16,28 +17,14 @@ Returns versions
 
 def version(state: StateManager[ModuleConfig], *args: list[str]) -> None:
     """
-    Return versions by looking for a pyproject.toml file in the parent directory of the module file.
+    Display the versions of the module.
     """
 
-    version = {}
+    version = get_version(state, *args)
 
-    app: App = state.app
-
-    if app._module:
-        module: Callable = app._module
-        module_file_path = inspect.getfile(module)
-        parent = os.path.dirname(module_file_path)
-
-        if not os.path.exists(os.path.join(parent, "pyproject.toml")):
-            parent = os.path.dirname(parent)
-
-        pyproject = os.path.join(parent, "pyproject.toml")
-
-        try:
-            with open(pyproject) as file:
-                version = toml.load(file)
-        except FileNotFoundError:
-            state.logger.debug(f"Could not find pyproject.toml in {parent}")
-            pass
-
-    state.last_ass_message.append(f"```json\n{json.dumps(version, indent=2, ensure_ascii=False)}")
+    if version is not None:
+        state.last_ass_message.append(
+            f"```json\n{json.dumps(version, indent=2, ensure_ascii=False)}"
+        )
+    else:
+        state.last_ass_message.append("Error fetching version!")
